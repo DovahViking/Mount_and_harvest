86cf02b761ebc542b9573f6ed6ffc1e4be28d5cf
Dovah Viking <DovahViking@hotmail.com>
2020-04-24 13:57:48 +0200

    Setup done~ish

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Mount_and_harvest
+{
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("test123", new TextObject("Click here", null), 9990, () => 
+            {
+                InformationManager.DisplayMessage(new InformationMessage("aaaaaaaaaa"));
+            }, false));
+        }
+    }
+}
