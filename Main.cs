a595e490aa7e8a18688525881db8b890230c1c98
Dovah Viking <DovahViking@hotmail.com>
2020-04-27 14:43:52 +0200

    Information gathering, docs, source code, etc.

--- Main.cs
+++ Main.cs
@@ -18,5 +18,12 @@ namespace Mount_and_harvest
                 InformationManager.DisplayMessage(new InformationMessage("aaaaaaaaaa"));
             }, false));
         }
+
+        // ----- 27/04/2020 -----
+
+        // Been reading the docs, the source code, as well as finding limitations to what's possible.
+        // Tomorrow I will continue from where I left, including trying out my own theories of how I can modify the things I want and add new content to the game
+        // else, if things are taking too long, I'll work on something else, and have this going as a side-project.
+
     }
 }
